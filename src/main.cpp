59eb02f7cb65742028e537951ca25bec1247495e
Lolbix69 <rbdproductionz@gmail.com>
2026-07-19 14:48:05 -0500

    added color support for individual vertices

--- src/main.cpp
+++ src/main.cpp
@@ -3,29 +3,38 @@
 #include <iostream>
 
 float vertices[] = {
-     0.5f,  0.5f, 0.0f, // top right
-     0.5f, -0.5f, 0.0f, // bottom right
-    -0.5f, -0.5f, 0.0f, // bottom left
-    -0.5f,  0.5f, 0.0f  // top left
+    0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // top right
+    -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom right
+    0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // top
 };
 
 unsigned int indices[] = {
-    0, 1, 3,
-    1, 2, 3
+    0, 1, 2,
+};
+
+float tex_coords[] = {
+    0.0f, 0.0f, //lower-left corner
+    1.0f, 0.0f, //lower-right corner
+    0.5f, 1.0f, //top_center corner
 };
 
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
+    "layout (location = 1) in vec3 aColor;\n"
+    "out vec3 ourColor;\n"
     "void main()\n"
     "{\n"
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "   ourColor = aColor;\n"
     "}\0";
 
 const char *fragShaderSource = "#version 330 core\n"
     "out vec4 FragColor;\n"
+    "in vec3 ourColor;"
     "void main()\n"
     "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);}\0";
+    "   FragColor = vec4(ourColor, 1.0f);\n"
+    "}\0";
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
@@ -73,8 +82,12 @@ int main() {
 
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    //position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, (6 * sizeof(float)), (void*)0);
     glEnableVertexAttribArray(0);
+    //color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, (6 * sizeof(float)), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     unsigned int EBO;
     glGenBuffers(1, &EBO);
@@ -125,7 +138,10 @@ int main() {
     glDeleteShader(vertex_shader);
     glDeleteShader(frag_shader);
 
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
     while(!glfwWindowShouldClose(window)) {
         //input
@@ -137,7 +153,7 @@ int main() {
         glUseProgram(shader_program);
         glBindVertexArray(VAO);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
 
         //check events and swap buffers
         glfwSwapBuffers(window);
